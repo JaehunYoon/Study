7e18df0f03e282c2afbe9e092ad2d04474e7f3dc
JaehunYoon <goodasd123@gmail.com>
2018-08-17 00:14:56 +0900

    Study

--- Web/PHP/SimpleBoard/index.php
+++ Web/PHP/SimpleBoard/index.php
@@ -1,14 +1,29 @@
 <!DOCTYPE html>
 <html>
-<head>
-    <meta charset="utf-8" />
-    <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <title>SimpleBoard</title>
-    <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
-    <script src="main.js"></script>
-</head>
-<body>
-    <h1>h4lo</h1>
-</body>
+    <head>
+        <meta charset="utf-8" />
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <title>SimpleBoard</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link rel="stylesheet" type="text/css" media="screen" href="main.css" />
+        <script src="main.js"></script>
+    </head>
+    <body>
+        <h1>h4lo</h1>
+        <hr>
+        <table border="1">
+            <tr>
+                <th>제목</th>
+                <td colspan="3">내용</td>
+                <td colspan="2">작성일</td>
+                <td colspan="2">작성자</td>
+            </tr>
+            <tr>
+                <th></th>
+                <td colspan="3">test</td>
+                <td colspan="2">2018-08-17</td>
+                <td colspan="2">h4lo</td>
+            </tr>
+        </table>
+    </body>
 </html>
