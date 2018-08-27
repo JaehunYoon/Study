f4218a7550814c004e826b199b2f8aca0ec670e1
JaehunYoon <goodasd123@gmail.com>
2018-08-27 12:04:10 +0900

    Study - C#

---
+++ Programming Language/C#/Example/01 Hello World.cs
@@ -0,0 +1,12 @@
+﻿using System; // 네임스페이스 System을 사용한다. (import 개념)
+
+namespace test // 네임스페이스(namespace)는 관련된 구조체, 클래스, 델리게이트, 인터페이스 등을 그룹화 한것을 말한다.
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!"); // Console.Write => 개행없이 출력 | Console.WriteLine => 출력 + 한 줄 개행
+        }
+    }
+}
