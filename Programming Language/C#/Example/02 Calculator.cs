1a4b58f9969af81eb94548e8c02c90e135297564
JaehunYoon <goodasd123@gmail.com>
2018-08-27 23:35:59 +0900

    Study - C#

---
+++ Programming Language/C#/Example/02 Calculator.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace test
+{
+    class Calculator
+    {
+        static void Main(string[] args)
+        {
+            while (true)
+            {              
+                Console.Write("Calc >> ");
+                string formula = Console.ReadLine();
+                Console.WriteLine(formula);
+
+                string[] split_formula = formula.Split(' ');
+
+                for (int i = 0; i < split_formula.Length; i++)
+                    Console.WriteLine(split_formula[i]);
+            }
+        }
+    }
+}
