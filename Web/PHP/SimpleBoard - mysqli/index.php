970fb990f7b5df56e753c865896e94b58841683a
JaehunYoon <goodasd123@gmail.com>
2018-09-16 23:43:41 +0900

    Study - PHP : Mysqli

---
+++ Web/PHP/SimpleBoard - mysqli/index.php
@@ -0,0 +1,12 @@
+<?php
+    include "./config.php";
+
+    $result = mysqli_query($conn, "SELECT * FROM table_name");
+    $row = mysqli_fetch_array($result, MYSQLI_ASSOC)
+//  OR
+    $row = mysqli_fetch_assoc($result);
+    echo row['id'];
+
+    mysqli_free_result($result);
+    mysqli_close($conn);
+?>
