f076c40451a80f52c03d74f0a8aa71141b3b0e0e
JaehunYoon <goodasd123@gmail.com>
2018-10-01 20:13:26 +0900

    Study - PHP : Object-Oriented Programming

--- Web/PHP/Example/04 object php/1 Basic Objects/Entree.php
+++ Web/PHP/Example/04 object php/1 Basic Objects/Entree.php
@@ -16,3 +16,28 @@ class Entree
         return in_array($ingredient, $this->ingredients);
     }
 }
+
+$soup = new Entree();  // $soup 에 Entree 객체 할당
+
+// $soup 속성 설정
+$soup -> name = 'Chicken Soup';
+$soup -> ingredients = array('Chicken', 'Water');
+
+$sandwich = new Entree();  // 또 다른 인스턴스 생성 -> $sandwich 에 할당
+
+// $sandwich 속성 설정
+$sandwich -> name = 'Chicken Sandwich';
+$sandwich -> ingredients = array('Chicken', 'Bread');
+
+foreach (['Chicken', 'Lemon', 'Bread', 'Water'] as $ing)
+{
+    if ($soup -> hasIngredient($ing))
+    {
+        echo "Materials to make soup : ". $ing . '<br>';
+    }
+
+    if ($sandwich -> hasIngredient($ing))
+    {
+        echo "Materials to make sandwich : ". $ing . '<br>';
+    }
+}
--- Web/PHP/Example/04 object php/1 Basic Objects/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-/**
- * Created by PhpStorm.
- * User: dsm2017
- * Date: 2018-10-01
- * Time: 오전 9:19
- */
