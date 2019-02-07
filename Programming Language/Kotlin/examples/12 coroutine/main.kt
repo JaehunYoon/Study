3dd46b498b2c351029fdcec9672e467ea5370055
JaehunYoon <goodasd123@gmail.com>
2019-02-07 17:23:10 +0900

    Study - Kotlin : Coroutine

---
+++ Programming Language/Kotlin/examples/12 coroutine/main.kt
@@ -0,0 +1,11 @@
+fun main() {
+    val lazySeq = sequence {
+        println("START")
+        for (i in 1..5) {
+            yield(i)
+            println("STEP")
+        }
+        println("END")
+    }
+    lazySeq.take(3).forEach { println(it)}
+}
