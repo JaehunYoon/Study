a297ca52a02760c9770985c8771d83c0244d9b1a
JaehunYoon <goodasd123@gmail.com>
2019-04-07 22:18:04 +0900

    Study - Python : DIY Hex Editor

--- Programming Language/Python/DIY Hxd/temp.py
+++ Programming Language/Python/DIY Hxd/temp.py
@@ -1,11 +1,31 @@
 import binascii
 
+# with open("bin/ex01.vhd", "rb") as f:
+#     f.seek(446)
+#     text = f.read(16)
+#     string = list(binascii.b2a_hex(text).upper().decode())
 
-lba_address = 0
+# for i in range(1, len(string), 2):
+#     string[i-1] += string[i]
+#     string[i] = ''
+
+# while '' in string:
+#     string.remove('')
+
+# print(" ".join(string))
+
+
+def nav_bar():
+    print("offset(h)", end=" ")
+    for i in range(16):
+        print(f"{hex(i)[2:].zfill(2).upper()}", end=" ")
+    print()
+    print("=" * 75, end='')
+    print()
 
 with open("bin/ex01.vhd", "rb") as f:
-    f.seek(446)
-    text = f.read(16)
+    f.seek(0)
+    text = f.read(512)
     string = list(binascii.b2a_hex(text).upper().decode())
 
 for i in range(1, len(string), 2):
@@ -15,4 +35,7 @@ for i in range(1, len(string), 2):
 while '' in string:
     string.remove('')
 
-print(" ".join(string))
+# print(" ".join(string))
+print(string[0:16])
+
+nav_bar()
