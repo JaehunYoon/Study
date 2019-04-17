63175654520553b0745788be707d8b38098b35ac
JaehunYoon <goodasd123@gmail.com>
2019-04-17 11:36:49 +0900

    Study - Python : DIY Hex Editor

--- Programming Language/Python/DIY Hxd/temp.py
+++ Programming Language/Python/DIY Hxd/temp.py
@@ -1,34 +1,9 @@
 import binascii
 
-with open("bin/ex01.vhd", "rb") as f:
-    f.seek(1468672 * 512 + 446)
-    text = f.read(32)
-    string = list(binascii.b2a_hex(text).upper().decode())
 
-for i in range(1, len(string), 2):
-    string[i-1] += string[i]
-    string[i] = ''
-
-while '' in string:
-    string.remove('')
-
-part_num = 1
-index = 0
-lba_addr = 0
-
-for index in range(0, 32, 16):
-    print(f"Partition [{part_num}]", end=" ")
-    part_num += 1
-    print(" ".join(string[index:index+16]), end=" ")
-    print()
-
-while True:
-    ext_index = 0
+def open_file(lba_addr):
     with open("bin/ex01.vhd", "rb") as f:
-        if index == 16:
-            f.seek(0 * 512 + 446 + 32)
-        else:
-            f.seek(lba_addr * 512 + 446)
+        f.seek(lba_addr * 512 + 446)
         text = f.read(32)
         string = list(binascii.b2a_hex(text).upper().decode())
 
@@ -38,22 +13,55 @@ while True:
 
     while '' in string:
         string.remove('')
-    
+
+    return string
+
+
+def print_part(string, index, part_num):
+    print(f"Partition [{part_num}]", end=" ")
+    print(" ".join(string[index:index+16]), end=" ")
+    print()
+
+string = open_file(0)
+
+part_num = 1
+index = 0
+lba_addr = 0
+
+for index in range(0, 32, 16):
+    print_part(string, index, part_num)
+    part_num += 1
+
+while True:
+    ext_index = 0
+    if index == 16:
+        with open("bin/ex01.vhd", "rb") as f:
+            f.seek(lba_addr * 512 + 446 + 32)
+            text = f.read(32)
+            string = list(binascii.b2a_hex(text).upper().decode())
+
+        for i in range(1, len(string), 2):
+            string[i-1] += string[i]
+            string[i] = ''
+
+        while '' in string:
+            string.remove('')
+        index = 9999
+    else:
+        string = open_file(lba_addr)
+
     while string[ext_index+4] != '00':
         if string[ext_index+4] == '05':
             temp = string[ext_index+8:ext_index+12]
             temp.reverse()
             lba_addr = int("".join(temp), 16)
-            print(lba_addr)
-            exit
-        print(f"Partition [{part_num}]", end=" ")
-        print(" ".join(string[ext_index:ext_index+16]), end=" ")
-        print()
-
-        part_num += 1
-        ext_index += 16
-        if ext_index == 32:
+            if lba_addr != 1259648:
+                lba_addr += 1259648
+            break
+        elif string[ext_index+4] != '05' and string[ext_index+20] != '00':
+            print_part(string, ext_index, part_num)
+            part_num += 1
+        else:
+            print_part(string, ext_index, part_num)
             exit()
-
-    print(string[ext_index: ext_index+16])
-    exit()
+        ext_index += 16
