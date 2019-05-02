bb9a58b6cf396cb3c12ce00cde2fdded747fcb93
JaehunYoon <goodasd123@gmail.com>
2019-05-02 13:38:57 +0900

    Study : Aseembly
    get kernel32.dll base address

--- Programming Language/C++/Example/03 shell_code/shellcode_cmd2.cpp
+++ Programming Language/C++/Example/03 shell_code/shellcode_cmd2.cpp
@@ -10,6 +10,15 @@ int _tmain(int argc, _TCHAR* argv[]) {
 		mov  byte ptr[ebp - 4], 63h
 		mov  byte ptr[ebp - 3], 6Dh
 		mov  byte ptr[ebp - 2], 64h
+
+		// kernel32.dll base address 구함
+		mov  eax, fs:[eax+0x30] // Get a pointer to the PEB
+		mov  eax, [eax+0xc] // Get PEB -> LDR (PEB_LDR_DATA)
+		mov  eax, [eax+0x14] // Get PEB -> LDR.InMemoryOrderModuleList (.exe InMemyroOrderModuleList)
+		mov  ebx, [eax] // Get the next entry (2nd entry) (ntdll.dll InMemoryOrderLinks)
+		mov  ebx, [ebx] // kernel32.dll InMemoryOrderLinks
+		mov  ebx, [ebx+0x10] // Get the 3rd entries base address (kernel32.dll) (ebx = kernel32.dll base address)
+		
 		// call WinExec('cmd', SW_SHOW)
 		push  5
 		lea  eax, [ebp - 4]
